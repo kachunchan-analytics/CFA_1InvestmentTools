38ce583ae72551171f282cd6f94afc3e16289fb4
kachunchan-analytics <kachunchan.a@gmail.com>
2024-07-01 17:28:07 +0800

    Updated new modules for learning

--- PUSH_TO_GITHUB.bat
+++ PUSH_TO_GITHUB.bat
@@ -1,20 +1,6 @@
 @echo off
 cd /d %cd%
 python change_TXT_to_PY.py
-git add . > nul
-if %errorlevel% NEQ 0 (
-  echo "Error: Git add failed!"
-  pause
-)
-
-git commit -m "Updated new modules for learning" > nul
-if %errorlevel% NEQ 0 (
-  echo "Error: Git commit failed!"
-  pause
-)
-
-git push > nul
-if %errorlevel% NEQ 0 (
-  echo "Error: Git push failed!"
-  pause
-)
+git add . 
+git commit -m "Updated new modules for learning"
+git push
