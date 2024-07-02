73581492e7f4fe69bfe5d347173eb8f476da1e42
kachunchan-analytics <kachunchan.a@gmail.com>
2024-07-02 12:40:37 +0800

    Updated new modules for learning

--- PUSH_TO_GITHUB.bat
+++ PUSH_TO_GITHUB.bat
@@ -1,5 +1,6 @@
 @echo off
-cd /d %cd%
+cd /d "C:\Users\Andy\Desktop\python_work_github\CFA_1InvestmentTools_GITHUB\CFA_1InvestmentTools"
+rem cd /d %cd%
 python change_TXT_to_PY.py
 git add . 
 git commit -m "Updated new modules for learning"
